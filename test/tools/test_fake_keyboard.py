fccd1310309763b64ad631f788647579555f5df5
Alon Levy <alon@pobox.com>
2017-09-07 13:03:01 +0300

    WIP NMEA python script to test FakeKeyboard

---
+++ test/tools/test_fake_keyboard.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import os
+import sys
+from socket import socket
+
+
+def checksum(s):
+    if s[0] == '$':
+        s = s[1:]
+    ret = ord(s[0])
+    for c in s[1:]:
+        ret ^= ord(c)
+    return ret
+
+
+def make_msg(key):
+    m = f'$FAKBD,{key}'
+    cksum = checksum(m)
+    m = f'{m}*{"%X" % cksum}'
+    return m
+
+def send(host, port, text):
+    s = socket()
+    s.connect((host, port))
+    f = s.makefile(mode='w')
+    for k in text:
+        f.write(make_msg(k) + '\n')
+        f.flush()
+
+
+if __name__ == '__main__':
+    send(host='localhost', port=10110, text='hello\n1234\ngoodbye\n')
+    #for x in sys.argv[1:]:
+    #    print(f'{x} => {make_msg(x)}')
